118784aa7b7ad1cb6912830f27fee9a26a885c7e
E:V:A <E3V3A@users.noreply.github.com>
2018-03-04 16:25:09 +0200

    Fixed spacing (tab to space)

--- MMM-ISS.js
+++ MMM-ISS.js
@@ -1,48 +1,46 @@
 /* Magic Mirror
  * Module: MMM-ISS
- * 
+ *
  * By Mykle1 - MIT Licensed
- * 
+ *
  */
+
 Module.register("MMM-ISS", {
 
     // Module config defaults.
     defaults: {
-		country: "United_States",                  // NO SPACES, USE UNDERSCORE
-		regionState: "Illinois",                   // NO SPACES, USE UNDERSCORE
-		city: "Chicago",                           // NO SPACES, USE UNDERSCORE
+        country: "United_States",                 // NO SPACES, USE UNDERSCORE
+        regionState: "Illinois",                  // NO SPACES, USE UNDERSCORE
+        city: "Chicago",                          // NO SPACES, USE UNDERSCORE
         lat: "",                                  // latitude
         lng: "",                                  // longitude
-		units: "mi",                              // mi = miles,mph, km = kilometers,
-        useHeader: false,                         // true if you want a header      
-        header: "Spot The Station",          // Any text you want. useHeader must be true
+        units: "mi",                              // mi = miles,mph, km = kilometers,
+        useHeader: false,                         // true if you want a header
+        header: "Spot The Station",               // Any text you want. useHeader must be true
         animationSpeed: 0,
         initialLoadDelay: 4250,
         retryDelay: 2500,
         updateInterval: 2 * 60 * 1000,
-
     },
 
     getStyles: function() {
         return ["MMM-ISS.css"];
     },
     getScripts: function() {
-	return ["moment.js"];	
-	},
+        return ["moment.js"];
+    },
 
     start: function() {
         Log.info("Starting module: " + this.name);
 
-
         //  Set locale.
         this.url = "http://api.open-notify.org/iss-pass.json?lat="  + this.config.lat +  "&lon="  + this.config.lng + "&n=5";
         this.ISS = {};
-		this.VELALT = {};
+        this.VELALT = {};
         this.scheduleUpdate();
     },
 
     getDom: function() {
-		
 
         var wrapper = document.createElement("div");
         wrapper.className = "wrapper";
@@ -60,49 +58,38 @@ Module.register("MMM-ISS", {
             header.innerHTML = this.config.header;
             wrapper.appendChild(header);
         }
-		
-			
-		var iframe = document.createElement("IFRAME");
-		iframe.style = "border:0";
-		iframe.width = "310px";
-		iframe.height = "450px";
-		iframe.src = 'https://spotthestation.nasa.gov/widget/widget.cfm?country=' + this.config.country + '&region=' + this.config.regionState + '&city=' + this.config.city + '&theme=2';
-		wrapper.appendChild(iframe);	
-		
-		
-		var ISS = this.ISS;
-		var VELALT = this.VELALT;
-		
-		
-		var velocity = document.createElement("div");
-            velocity.classList.add("xsmall", "bright", "velocity");
-			if (this.config.units != "km"){
-				velocity.innerHTML = "Velocity: " + Math.round(VELALT.velocity * 0.621371) + " mph &nbsp &nbsp &nbsp " + " Altitude: " + Math.round(VELALT.altitude * 0.621371) + " miles";
-			} else {
-				velocity.innerHTML = "Velocity: " + Math.round(VELALT.velocity) + " km/h &nbsp &nbsp &nbsp " + " Altitude: " + Math.round(VELALT.altitude) + " km";
-			}
-            wrapper.appendChild(velocity);
 
-		return wrapper;
-		
-		
+        var iframe = document.createElement("IFRAME");
+        iframe.style = "border:0";
+        iframe.width = "310px";
+        iframe.height = "450px";
+        iframe.src = 'https://spotthestation.nasa.gov/widget/widget.cfm?country=' + this.config.country + '&region=' + this.config.regionState + '&city=' + this.config.city + '&theme=2';
+        wrapper.appendChild(iframe);
+
+        var ISS = this.ISS;
+        var VELALT = this.VELALT;
+        var velocity = document.createElement("div");
+        velocity.classList.add("xsmall", "bright", "velocity");
+        if (this.config.units != "km"){
+            velocity.innerHTML = "Velocity: " + Math.round(VELALT.velocity * 0.621371) + " mph &nbsp &nbsp &nbsp " + " Altitude: " + Math.round(VELALT.altitude * 0.621371) + " miles";
+        } else {
+            velocity.innerHTML = "Velocity: " + Math.round(VELALT.velocity) + " km/h &nbsp &nbsp &nbsp " + " Altitude: " + Math.round(VELALT.altitude) + " km";
+        }
+        wrapper.appendChild(velocity);
+        return wrapper;
     },
-	
 
     processISS: function(data) {
         this.ISS = data;
-	//	console.log(this.ISS); // for checking in dev console
+        //console.log(this.ISS); // for checking in dev console
         this.loaded = true;
     },
-	
-	
-	processVELALT: function(data) {
+
+    processVELALT: function(data) {
         this.VELALT = data;
-	//	console.log(this.VELALT); // for checking in dev console
+        //console.log(this.VELALT); // for checking in dev console
         this.loaded = true;
     },
-	
-	
 
     scheduleUpdate: function() {
         setInterval(() => {
@@ -120,12 +107,12 @@ Module.register("MMM-ISS", {
             this.processISS(payload);
             this.updateDom(this.config.animationSpeed);
         }
-		
-		if (notification === "VELALT_RESULT") {
+
+        if (notification === "VELALT_RESULT") {
             this.processVELALT(payload);
             this.updateDom(this.config.fadeSpeed);
         }
-		
+
         this.updateDom(this.config.initialLoadDelay);
     },
 });
